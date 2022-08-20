0737a9525a4160e5bf64571ca573d9818595f7be
kATLEGO47 <2269574@students.wits.ac.za>
2022-08-21 01:26:41 +0200

    Middleware for auth,admin,validUsers

---
+++ server/middleware/admin.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req,res,next) =>{
+    const token = req.hearder("x-auth-token");
+    if(!token){
+        return res.status(400).send({message:"Access denied,no token provided"});
+    }
+
+    jwt.verify(token,process.env.JWTPRIVATEKEY,(error,validToken)=>{
+        if(error){
+            return res.status(400).send({message:"invalid token"});
+        }else{
+            if(!validToken.isAdmin){
+                return res.status(403).send({message:"You don't have access to this content"});
+            }
+            req.user = validToken;
+            next();
+        }
+    })
+}
